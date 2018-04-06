8d5b45271e6eadbf47d0f6ec7325f6cdde75d7d5
jihadaamalia <jihadaamalia@yahoo.com>
2018-04-06 16:09:29 +0700

    reset password directly

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,9 @@
 'use strict';
 
 module.exports = function(app) {
- 	var user = require('../controllers/userController'); //to get something outside the caller file folder
     var signup = require('../controllers/signUpController');
     var signin = require('../controllers/signInController');
-    var location = require('../controllers/locationController');
+    //var location = require('../controllers/locationController');
     var passhandler = require('../controllers/passHandlerController');
 
   	app.get("/",function(req,res){
@@ -19,8 +18,8 @@ module.exports = function(app) {
 	app.post('/signin', signin.user);
 
 	//get location
-    app.get('/provinces', location.provinces); //get province
-    app.get('/provinces/:prov_id', location.cities); //get city by province
+    // app.get('/provinces', location.provinces); //get province
+    // app.get('/provinces/:prov_id', location.cities); //get city by province
 
 	//forgot password
 	app.post('/forgot_password', passhandler.forgot_password);
@@ -28,9 +27,6 @@ module.exports = function(app) {
 	    .get(passhandler.render_reset_password_template) //get website page to input new pass
 	    .post(passhandler.reset_password); //update password
 	app.get('/reset_password/success', passhandler.render_reset_password_success_template);
-
-    //additional feature
-    app.post('/verify_access', user.verify); //TODO move this code to app server
 
 };
   
